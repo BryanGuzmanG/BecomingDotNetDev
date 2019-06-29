2f6c9068ad1ff489e49dc5952105937b95dc358b
Bryan Guzman <bryanguzman18@outlook.com>
2019-06-29 15:05:35 -0400

    Update

---
+++ EventsCoach/EcCoach.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EcCoach.Web.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EcCoach.Web.Areas.Identity.IdentityHostingStartup))]
+namespace EcCoach.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DataContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DataContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<DataContext>();
+            });
+        }
+    }
+}
